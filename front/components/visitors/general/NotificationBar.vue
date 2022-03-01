0ed0315292b45b85ec7fd4db57c93aee73a4eee7
kofi-teddy <agudogot@gmail.com>
2022-03-01 15:29:55 +0000

    setup navigation components

---
+++ front/components/visitors/general/NotificationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+        <div
+            v-for="i in notificationErrors"
+            :key="i.id"
+            :class="'full-background ' + i.color"
+            >
+            {{i.text}}
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotificationBar',
+  data: () => ({
+    notificationErrors: [{ 'color': 'red', id: 0, text: 'Oh, we are having some issues' }]
+  })
+}
+</script>
+
+<style scoped>
+.full-background {
+  padding: 10px;
+  z-index: 0;
+  position: relative;
+  text-align: center;
+  color: white;
+}
+
+</style>
