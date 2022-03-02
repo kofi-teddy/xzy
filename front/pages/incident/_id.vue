2212812c9aa957b6da75fca549b55af3d11f1415
kofi-teddy <agudogot@gmail.com>
2022-03-02 16:26:42 +0000

    incident detail fixes

--- front/pages/incident/_id.vue
+++ front/pages/incident/_id.vue
@@ -26,6 +26,7 @@
 
 <script>
 export default {
+    name: 'IncidentDetail',
     data: () => ({
         incidents: [{ id: 1, title: 'Connectivity issues', update_set: [{ title: 'Connectivity issues: slow response time API', description: 'We are experiencing connectivity issues on our API. We are investigating this issue and will keep you up to date on this.', date: '2018-09-12 12:30:00', status: 'identified'}, { title: 'We are still investigating this issue.', description: 'We are still working hard on investigating this issue. We appreciate your support and patience.', date: '2018-08-23 12:23:23', status: 'investigating'}] }],
         incident: []
