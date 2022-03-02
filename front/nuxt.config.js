14eb02dfbbdd930c2ff251a781e1f59b433ac0c6
kofi-teddy <agudogot@gmail.com>
2022-03-02 10:34:33 +0000

    font family montserrat

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -21,7 +21,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700' }
     ]
   },
 
