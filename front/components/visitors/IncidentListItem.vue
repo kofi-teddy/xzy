e6ba24b2a91c85045e03487e18d5a9555e49e085
kofi-teddy <agudogot@gmail.com>
2022-03-02 13:08:12 +0000

    incidentListItem

---
+++ front/components/visitors/IncidentListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{ incident.title }}</h2>
+    <p>{{ lastUpdate.description }}</p>
+    <div class="sub">
+      {{ lastUpdate.date }}
+      <span class="right">
+        Status: {{ lastUpdate.status }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Incident',
+    props: {
+        incident: {
+        type: Object,
+        default: () => { return {} }
+        }
+    },
+    computed: {
+        lastUpdate () {
+        return this.incident.update_set[0]
+        }
+    }
+}
+</script>
+
+<style>
+    .sub {
+    color: #999999;
+    }
+    .right {
+    float: right;
+    }
+    h2 {
+    margin-bottom: 10px;
+    }
+</style>
