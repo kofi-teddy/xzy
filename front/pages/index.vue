da36275a9cc58decd9f5ade2e79aab8ca911aa3e
kofi-teddy <agudogot@gmail.com>
2022-03-02 13:20:51 +0000

    incident list item and cards styles

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-
+  <div>
+    <div class="cards">
+      <div
+        v-if="openIncidents.length > 0" 
+        class="card incidents" 
+      >
+        <div
+          v-for="i in openIncidents"
+          :key="i.id"
+          class="incident_item"
+        >
+          <nuxt-link :to="{ name: 'incident-id', params: { id: i.id }}">
+            <IncidentListItem :incident="i" />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import IncidentListItem from '@/components/visitors/IncidentListItem'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { IncidentListItem },
+  data: () => ({
+    openIncidents: [{ id: 1, title: 'Connectivity Issues', update_set: [{ description: 'We have noticed some connectivity issues', date: '2019-10-02 13:00:12', status: 'Investigating' }] }]
+  }),
 }
 </script>
