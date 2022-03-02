60bb3307ed0a02913f563254f39471ca525aec92
kofi-teddy <agudogot@gmail.com>
2022-03-02 12:14:07 +0000

    indcatorIcon

---
+++ front/components/general/IndicatorIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <div :class="'indicator ' + status" />
+        <p v-if="showText">
+        {{ message }}
+        </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndicatorIcon',
+  props: {
+    status: {
+        type: String,
+        default: 'up',
+        validator (value) {
+        return ['up', 'down', 'issue'].includes(value)
+        }
+    },
+    showText: Boolean
+  },
+  computed: {
+    message () {
+      if (this.status === 'up') {
+        return 'All is good'
+      } else if (this.status === 'down') {
+        return 'We are having issues'
+      } else if (this.status === 'issue') {
+        return 'You might notice some inconvenience'
+      } else {
+        return 'Invalid'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
