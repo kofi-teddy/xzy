7961dd9fc151504a38e68d319cab1f141c631f11
kofi-teddy <agudogot@gmail.com>
2022-03-05 22:53:13 +0000

    fix custom user model

--- back/apps/users/models.py
+++ back/apps/users/models.py
@@ -5,6 +5,34 @@ from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
 
+class CustomUserManager(BaseUserManager):
+    
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        if not email:
+            raise ValueError('The given email  must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            date_joined=datetime.now(), 
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+
+
+
+
 class User(AbstractBaseUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -28,34 +56,10 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         return self.is_superuser
     
+    object = CustomUserManager()
 
 def save(self, *args, **kwargs):
     self.email = self.email.lower()
     return super(User, self).save(*args, **kwargs) 
 
-
-class CustomUserManager(BaseUserManager):
-    
-    def create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        if not email:
-            raise ValueError('The given email  must be set')
-        
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            is_staff=is_staff,
-            is_active=True,
-            is_superuser=is_superuser,
-            date_joined=datetime.now(), 
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, **extra_fields)
 
