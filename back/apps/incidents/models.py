757c5a55b68d00c79d7a60e459915f1d59a29502
kofi-teddy <agudogot@gmail.com>
2022-03-05 23:27:35 +0000

    incident docs

--- back/apps/incidents/models.py
+++ back/apps/incidents/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Incident(models.Model):
+    '''
+    An Incident is an event that is an inconvenience, be it downtime or maintenance.
+    An Incident is linked to Update, as an Incident should contain multiple Updates 
+    to keep the users up to date on the latest progress. An Incident should also be linked to a Site, since we could have multiple sites to watch and we want to know which site has an issue.
+    '''
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     start = models.DateTimeField(auto_now_add=True)
