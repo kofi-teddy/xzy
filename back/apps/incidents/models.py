979cb070562177351dc8775c2ff842299b66f0dd
kofi-teddy <agudogot@gmail.com>
2022-03-05 23:26:02 +0000

    incident model

--- back/apps/incidents/models.py
+++ back/apps/incidents/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Incident(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
