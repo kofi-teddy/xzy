072d935259c65056e2e6cbcc3a705538163df926
kofi-teddy <agudogot@gmail.com>
2022-03-05 19:58:22 +0000

    added custom user to settings

--- back/back/settings.py
+++ back/back/settings.py
@@ -114,3 +114,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user
+AUTH_USER_MODEL = 'apps.users.User'
