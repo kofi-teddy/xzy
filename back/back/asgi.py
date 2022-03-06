e322330e53aa2fbf3a285ca9f71a5235abdce75a
kofi-teddy <agudogot@gmail.com>
2022-03-06 00:01:36 +0000

    updated asgi

--- back/back/asgi.py
+++ back/back/asgi.py
@@ -3,11 +3,17 @@ ASGI config for back project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 """
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from incidents import routing
+from django.core.asgi import get_wsgi_application
 
-import os
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
-
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_wsgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
+})
