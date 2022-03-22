e4e83bc32f9ba946166dc443b1cf97f86e60996d
kofi-teddy <agudogot@gmail.com>
2022-03-22 18:03:39 +0000

    added websockets to nuxt config

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -32,7 +32,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/fontawesome'
+    '~plugins/fontawesome',
+    '~plugins/websockets',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
