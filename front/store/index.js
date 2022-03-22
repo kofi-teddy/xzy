5c138cbda3ab97461ae3324b8a08b13dadd013c2
kofi-teddy <agudogot@gmail.com>
2022-03-22 18:15:58 +0000

    added mutations to store

--- front/store/index.js
+++ front/store/index.js
@@ -1,5 +1,27 @@
 export const state = () => ({});
 
-export const mutations = {};
+export const mutations = {
+    SOCKET_ONOPEN(state, event) {
+        console.log('What can I do for you?')
+        state.socket.isConnected = true
+    },
+    SOCKET_ONCLOSE(state, event) {
+        console.log('Oh dang I cant reach you anymore!')
+        state.socket.isConnected = true
+    },
+    SOCKET_ONERROR(state, event) {
+        console.error(state, event)
+    },
+    SOCKET_ONMESSAGE (state, message) {
+        state.socket.message = message
+    },
+    SOCKET_RECONNECT (state, message) {
+        state.socket.message = message
+    },
+    SOCKET_RECONNECT_ERROR (state) {
+        console.log('we are back up and sailing guys! Nothing to worry about.')
+        state.socket.reconnectionError = true
+    }
+};
 
 export const actions = {};
