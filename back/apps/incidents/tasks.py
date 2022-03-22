de8e2fe6aae94d59922e083e3e49be7b881f9c3b
kofi-teddy <agudogot@gmail.com>
2022-03-22 19:02:26 +0000

    check all the sites

--- back/apps/incidents/tasks.py
+++ back/apps/incidents/tasks.py
@@ -1,6 +1,38 @@
-from celery import shared_tasks
+from celery import shared_task
+import requests
+from datetime import datetime 
+from request.exceptions import ConnectionError, Timeout
+
+from incidents.models import Site, Uptime
+
+
+
+sites = Site.objects.all()
+for site in sites:
+    try:
+        r = requests.head(site.url, timeout=4)
+        Uptime.objects.create(
+            site=site, status='up',
+            response_time=int(r.elapsed.total_seconds() * 1000),
+            date=datetime.now()
+            )
+    except Timeout as e:
+        Uptime.objects.create(
+            site=site, 
+            status='up',
+            response_time=4000,
+            date=datetime.now())
+    except Exception as e:
+        Uptime.objects.create(
+            site=site, 
+            status='down', 
+            response_time=0, 
+            date=datetime.now()
+            )
 
 
 @shared_task
 def update_to_visitor():
     print('Task update_to_visitor has been initiated')
+
+
