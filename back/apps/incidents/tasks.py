cbaf233d29f1ffda4632ba8e146b25d3ff099047
kofi-teddy <agudogot@gmail.com>
2022-03-22 19:15:38 +0000

    fix bugs and documents code

--- back/apps/incidents/tasks.py
+++ back/apps/incidents/tasks.py
@@ -9,37 +9,41 @@ from incidents.models import Site, Uptime
 
 
 
-sites = Site.objects.all()
-for site in sites:
-    try:
-        r = requests.head(site.url, timeout=4)
-        Uptime.objects.create(
-            site=site, status='up',
-            response_time=int(r.elapsed.total_seconds() * 1000),
-            date=datetime.now()
-            )
-    except Timeout as e:
-        Uptime.objects.create(
-            site=site, 
-            status='up',
-            response_time=4000,
-            date=datetime.now())
-    except Exception as e:
-        Uptime.objects.create(
-            site=site, 
-            status='down', 
-            response_time=0, 
-            date=datetime.now()
-            )
 
-channel_layer = get_channel_layer()
-async_to_sync(channel_layer.group_send)(
-    'All', {'type': 'chat_message', 'message': data}
-)
 
 
 @shared_task
 def update_to_visitor():
     print('Task update_to_visitor has been initiated')
+    sites = Site.objects.all()
+
+    # ping all sites
+    for site in sites:
+        try:
+            r = requests.head(site.url, timeout=4)
+            Uptime.objects.create(
+                site=site, status='up',
+                response_time=int(r.elapsed.total_seconds() * 1000),
+                date=datetime.now()
+                )
+        except Timeout as e:
+            Uptime.objects.create(
+                site=site, 
+                status='up',
+                response_time=4000,
+                date=datetime.now())
+        except Exception as e:
+            Uptime.objects.create(
+                site=site, 
+                status='down', 
+                response_time=0, 
+                date=datetime.now()
+                )
+
+    # send messages to groups
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'All', {'type': 'chat_message', 'message': data}
+    )
 
 
