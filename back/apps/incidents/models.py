83f2125fca546dfab2c608a1f530b9168ca15b5e
kofi-teddy <agudogot@gmail.com>
2022-03-22 17:48:04 +0000

    models updated

--- back/apps/incidents/models.py
+++ back/apps/incidents/models.py
@@ -24,7 +24,7 @@ class Incident(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     start = models.DateTimeField(auto_now_add=True)
-    end = models.BooleanField(default=False)
+    end = models.DateTimeField(null=True, blank=True)
     solved = models.BooleanField(default=False)
 
     def __str__(self):
