1d5bc24ebb29bd456c4302a8153051a6b665d3b7
kofi-teddy <agudogot@gmail.com>
2022-03-23 10:30:05 +0000

    fix imports

--- back/apps/incidents/admin.py
+++ back/apps/incidents/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from incidents.models import Site, Uptime, Incident, Update
+from apps.incidents.models import Site, Uptime, Incident, Update
 
 
 admin.site.register(Site)
