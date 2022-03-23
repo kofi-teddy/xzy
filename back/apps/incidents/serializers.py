42c962ef3a3b51cc51880c44ce98040b04c89829
kofi-teddy <agudogot@gmail.com>
2022-03-23 09:28:19 +0000

    update serializer

--- back/apps/incidents/serializers.py
+++ back/apps/incidents/serializers.py
@@ -7,3 +7,11 @@ class SiteSerializer(serializers.ModelSerilizer):
     class Meta:
         model = Site
         fields = ('id', 'title', 'url', 'date')
+
+
+class UpdateSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(source='get_status_display')
+
+    class Meta:
+        model = Update
+        fields = ('id', 'description', 'status', 'date')
