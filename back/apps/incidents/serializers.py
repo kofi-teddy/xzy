482401389151983237a3f3efcc99c7f41195790a
kofi-teddy <agudogot@gmail.com>
2022-03-23 09:35:27 +0000

    site serializer

--- back/apps/incidents/serializers.py
+++ back/apps/incidents/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from incidents.models import Incident, Site, Update, Uptime
 
 
-class SiteSerializer(serializers.ModelSerilizer):
-    class Meta:
-        model = Site
-        fields = ('id', 'title', 'url', 'date')
-
-
 class UpdateSerializer(serializers.ModelSerializer):
     status = serializers.CharField(source='get_status_display')
 
@@ -23,3 +17,12 @@ class IncidentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Incident
         fields = ('id', 'title', 'update_set', 'start', 'end', 'solved')
+
+
+class SiteSerializer(serializers.ModelSerilizer):
+    uptime_set = UptimeSerializer(many=True, read_only=True)
+    incident_set = IncidentSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Site
+        fields = ('id', 'title', 'url', 'date')
