cded73d32c25cd3cb8344c0073061b0740595a51
kofi-teddy <agudogot@gmail.com>
2022-03-24 10:17:53 +0000

    clean up

--- front/pages/incident/_id.vue
+++ front/pages/incident/_id.vue
@@ -43,9 +43,6 @@ export default {
         incidents: [{ id: 1, title: 'Connectivity issues', update_set: [{ title: 'Connectivity issues: slow response time API', description: 'We are experiencing connectivity issues on our API. We are investigating this issue and will keep you up to date on this.', date: '2022-03-2 12:30:00', status: 'identified'}, { title: 'We are still investigating this issue.', description: 'We are still working hard on investigating this issue. We appreciate your support and patience.', date: '2022-03-2 12:23:23', status: 'investigating'}] }],
         incident: []
     }),
-    mounted () {
-        this.incident = this.incidents.find(a => a.id === parseInt(this.$route.params.id))
-    },
     computed: {
       incident () {
         return this.$store.state.incidents.find(a => a.id === parseInt(this.$route.params.id))
