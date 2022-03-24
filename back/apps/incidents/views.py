c68d5202e4660690a047f3f0a90534004ea72b33
kofi-teddy <agudogot@gmail.com>
2022-03-24 10:49:26 +0000

    subscriberview

--- back/apps/incidents/views.py
+++ back/apps/incidents/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, mixins, viewsets
+from rest_framework import Response
+
+from incidents.serializers import SubscriberSerializer
+
+
+class SubscriberView(mixins.CreateModelMixin, viewsets.GenericViewset):
+    permission_classes = (permissions.AllowAny)
+    serializer_class = SubscriberSerializer
