41c0a5ec856038669ace8ef450c46e76806e135b
kofi-teddy <agudogot@gmail.com>
2022-03-30 13:56:13 +0000

    fix views erros

--- back/apps/incidents/views.py
+++ back/apps/incidents/views.py
@@ -5,6 +5,6 @@ from rest_framework import permissions, mixins, viewsets
 from apps.incidents.serializers import SubscriberSerializer
 
 
-class SubscriberView(mixins.CreateModelMixin, viewsets.GenericViewset):
+class SubscriberView(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (permissions.AllowAny)
     serializer_class = SubscriberSerializer
